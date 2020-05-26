5f81e784690e2580c82cb020a1441f145b124ed4
Frieda Musa <frieda@Friedas-MacBook-Air.local>
2020-05-25 22:59:56 -0230

    started tribute1

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -30,40 +30,90 @@ $(function () {
                     message: message,
                 },
                 cache: false,
-                success: function () {
-                    // Success message
-                    $("#success").html("<div class='alert alert-success'>");
-                    $("#success > .alert-success")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
-                    );
-                    $("#success > .alert-success").append("</div>");
-                    //clear all fields
-                    $("#tributeForm").trigger("reset");
+                success: postSuccess,
+                error: postError,
+                complete: function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+                    }, 1000);
                 },
-                error: function () {
-                    // Fail message
-                    $("#success").html("<div class='alert alert-danger'>");
-                    $("#success > .alert-danger")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-danger").append(
-                        $("<strong>").text(
-                            "Sorry " +
-                                firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
-                        )
-                    );
-                    $("#success > .alert-danger").append("</div>");
-                    //clear all fields
-                    $("#tributeForm").trigger("reset");
-        }
+            });
+            function postSuccess(data, textStatus, jqXHR) {
+                // Success message
+                $("#success").html("<div class='alert alert-success'>");
+                $("#success > .alert-success")
+                    .html(
+                        "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                    )
+                    .append("</button>");
+                $("#success > .alert-success").append(
+                    "<strong>Your message has been sent. </strong>"
+                );
+                $("#success > .alert-success").append("</div>");
+                //clear all fields
+                $("#tributeForm").get(0).reset();
+                displayComment(data)
+            };
+            function displayComment(data) {
+                var commentHtml = createComment(data);
+                var commentEl = $(commentHtml);
+                commentEl.hide();
+                var postsList = $('#posts-list');
+                postsList.addClass('has-comments');
+                postsList.prepend(commentEl);
+                commentEl.slideDown();
+            }
+              
+            function postError(jgXHR, textStatus, errorThrown) {
+                // Fail message
+                $("#success").html("<div class='alert alert-danger'>");
+                $("#success > .alert-danger")
+                    .html(
+                        "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                    )
+                    .append("</button>");
+                $("#success > .alert-danger").append(
+                    $("<strong>").text(
+                        "Sorry " +
+                            firstName +
+                            ", it seems that my mail server is not responding. Please try again later!"
+                    )
+                );
+                $("#success > .alert-danger").append("</div>");
+                //clear all fields
+                $("#tributeForm").trigger("reset");
+            }
+
+            function createComment(data) {
+                var html = ’ +
+                '<li><article id="' + data.id + '" class="hentry">' +
+                  '<footer class="post-info">' +
+                    '<abbr class="published" title="' + data.date + '">' +
+                      parseDisplayDate(data.date) +
+                    '</abbr>' +
+                    '<address class="vcard author">' +
+                      'By <a class="url fn" href="#">' + data.name + '</a>' +
+                    '</address>' +
+                  '</footer>' +
+                  '<div class="entry-content">' +
+                    '<p>' + data.comment + '</p>' +
+                  '</div>' +
+                '</article></li>';
+              
+                return html;
+              }
+              
+              function parseDisplayDate(date) {
+                date = (date instanceof Date? date : new Date( Date.parse(date) ) );
+                var display = date.getDate() + ' ' +
+                              ['January', 'February', 'March',
+                               'April', 'May', 'June', 'July',
+                               'August', 'September', 'October',
+                               'November', 'December'][date.getMonth()] + ' ' +
+                              date.getFullYear();
+                return display;
+              }
+        },
         filter: function () {
             return $(this).is(":visible");
         },
