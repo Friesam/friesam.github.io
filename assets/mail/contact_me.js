5e50dd1f8db058881479cc56f15a837c229c4f7a
Frieda Musa <frieda@Friedas-MacBook-Air.local>
2020-05-25 23:55:14 -0230

    adding post

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -32,16 +32,41 @@ $(function () {
                 cache: false,
                 success: function () {
                     // Success message
-                    $("#success").html("<div class='alert alert-success'>");
-                    $("#success > .alert-success")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
-                    );
-                    $("#success > .alert-success").append("</div>");
+                    // $("#success").html("<div class='alert alert-success'>");
+                    // $("#success > .alert-success")
+                    //     .html(
+                    //         "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                    //     )
+                    //     .append("</button>");
+                    // $("#success > .alert-success").append(
+                    //     "<strong>Your message has been sent. </strong>"
+                    // );
+                    // $("#success > .alert-success").append("</div>");
+                    $(document).ready(function() {
+                        $('button').click(function() {
+                            var comment = $('.commentBox').val();
+                            $('<li>').text(comment).prependTo('.comments');
+                            $('button').attr('disabled', 'true');
+                            $('.commentBox').val('');
+                        });
+                        
+                        $('.commentBox').keyup(function() {
+                            var commentLength = $(this).val().length;
+                            var charLeft =  1200 - commentLength;
+                            $('.counter').text(charLeft);
+                            
+                            if (commentLength == 0) {
+                                $('button').attr('disabled', 'true');
+                            }
+                        
+                            else {
+                                $('button').removeAttr('disabled', 'true');
+                            }
+                        });
+                        
+                        $('button').attr('disabled', 'true');
+                        
+                    });
                     //clear all fields
                     $("#tributeForm").trigger("reset");
                 },
